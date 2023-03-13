ac44971d2f5fe99ca6ee2763ad344e3262d97ea9
dylanclements <58539346+dylanclements@users.noreply.github.com>
2023-03-13 14:09:54 +1100

    add tailwind to vite

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { TRPCProvider } from "./utils/api";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TRPCProvider>
+      <div className="flex h-screen w-screen items-center justify-center">
+        Hello World
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </TRPCProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ apps/vite/src/utils/api.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import superjson from "superjson";
+
+import type { AppRouter } from "@acme/api";
+
+export { type RouterInputs, type RouterOutputs } from "@acme/api";
+
+const api = createTRPCReact<AppRouter>();
+
+const getBaseUrl = () => {
+  // make this connect to api url in the future
+  return "https://localhost:3000";
+};
+
+/**
+ * A wrapper for your app that provides the TRPC context.
+ * Use only in _app.tsx
+ */
+export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    api.createClient({
+      transformer: superjson,
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    }),
+  );
+
+  return (
+    <api.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </api.Provider>
+  );
+};
