466800b69bd75d2dec82b1a2934ed365b7a21ec7
dylanclements <58539346+dylanclements@users.noreply.github.com>
2023-03-13 15:33:40 +1100

    remove trpc from vite

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,12 +1,8 @@
-import { TRPCProvider } from "./utils/api";
-
 function App() {
   return (
-    <TRPCProvider>
-      <div className="h-screen w-screen flex-col items-center justify-center">
-        <span>Hello World</span>
-      </div>
-    </TRPCProvider>
+    <div className="h-screen w-screen flex-col items-center justify-center">
+      <span>Hello World</span>
+    </div>
   );
 }
 
