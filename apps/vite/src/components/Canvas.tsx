167a5ac769049c2fb580959f36bff6306ae33d4b
dylanclements <58539346+dylanclements@users.noreply.github.com>
2023-03-27 09:38:08 +1100

    prefix all event with _

--- apps/vite/src/components/Canvas.tsx
+++ apps/vite/src/components/Canvas.tsx
@@ -9,42 +9,42 @@ const Canvas = () => {
   // const canvasWidth = 800 * canvasScale; // temporarily hardcoded values
 
   const handleContextMenu: (event: React.MouseEvent) => void = (
-    event: React.MouseEvent,
+    _event: React.MouseEvent,
   ) => {
     console.warn("handleContextMenu triggered - NOT YET IMPLMENTED");
     return;
   };
 
   const handlePointerDown: (event: React.PointerEvent) => void = (
-    event: React.PointerEvent,
+    _event: React.PointerEvent,
   ) => {
     console.warn("handlePointerDown triggered - NOT YET IMPLMENTED");
     return;
   };
 
   const handleDoubleClick: (event: React.MouseEvent) => void = (
-    event: React.MouseEvent,
+    _event: React.MouseEvent,
   ) => {
     console.warn("handleDoubleClick triggered - NOT YET IMPLMENTED");
     return;
   };
 
   const handlePointerMove: (event: React.PointerEvent) => void = (
-    event: React.PointerEvent,
+    _event: React.PointerEvent,
   ) => {
     console.warn("handlePointerMove triggered - NOT YET IMPLMENTED");
     return;
   };
 
   const handlePointerUp: (event: React.PointerEvent) => void = (
-    event: React.PointerEvent,
+    _event: React.PointerEvent,
   ) => {
     console.warn("handlePointerUp triggered - NOT YET IMPLMENTED");
     return;
   };
 
   const removePointer: (event: React.PointerEvent) => void = (
-    event: React.PointerEvent,
+    _event: React.PointerEvent,
   ) => {
     console.warn("removePointer triggered - NOT YET IMPLMENTED");
     return;
@@ -52,7 +52,7 @@ const Canvas = () => {
 
   const handleTouchMove: (
     event: React.TouchEvent<HTMLCanvasElement>,
-  ) => void = (event: React.TouchEvent<HTMLCanvasElement>) => {
+  ) => void = (_event: React.TouchEvent<HTMLCanvasElement>) => {
     console.warn("handleTouchMove triggered - NOT YET IMPLMENTED");
     return;
   };
