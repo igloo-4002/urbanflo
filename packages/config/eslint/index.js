dd86052ab70be41ea4810bb5e4272a42c1955b93
dylanclements <58539346+dylanclements@users.noreply.github.com>
2023-03-27 11:19:22 +1100

    add ts files to ignore

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -23,7 +23,12 @@ const config = {
       { prefer: "type-imports", fixStyle: "inline-type-imports" },
     ],
   },
-  ignorePatterns: ["**/*.config.js", "**/*.config.cjs", "packages/config/**"],
+  ignorePatterns: [
+    "**/*.config.js",
+    "**/*.config.cjs",
+    "**/*.config.ts",
+    "packages/config/**",
+  ],
   reportUnusedDisableDirectives: true,
 };
 
