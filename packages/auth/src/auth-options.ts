827222fadde8241ef00ccd655203fda402289cff
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-03-27 11:17:15 +1100

    Remove all mentions of discord auth

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -42,10 +42,6 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    // DiscordProvider({
-    //   clientId: process.env.DISCORD_CLIENT_ID as string,
-    //   clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
-    // }),
     /**
      * ...add more providers here
      *
