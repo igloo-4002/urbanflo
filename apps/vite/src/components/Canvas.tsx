539e76caf6f72941194bbf22159bae42f79e8064
dylanclements <58539346+dylanclements@users.noreply.github.com>
2023-04-03 12:06:59 +1000

    add drivable car

--- apps/vite/src/components/Canvas.tsx
+++ apps/vite/src/components/Canvas.tsx
@@ -4,13 +4,16 @@ import { Image, Layer, Stage } from "react-konva";
 
 import carImage from "../assets/car.png";
 
-const CarCanvas: React.FC = () => {
+export default function Canvas() {
   const [car, setCar] = useState<HTMLImageElement | null>(null);
   const carRef = useRef<Konva.Image>(null);
+  const keysPressed = useRef<{ [key: string]: boolean }>({});
 
   useEffect(() => {
     const image = new window.Image();
     image.src = carImage;
+    image.width = 50;
+    image.height = 125;
     image.onload = () => {
       setCar(image);
     };
@@ -25,36 +28,57 @@ const CarCanvas: React.FC = () => {
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
+      keysPressed.current[e.key] = true;
+    };
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      keysPressed.current[e.key] = false;
+    };
+
+    const moveCar = () => {
       if (!carRef.current) return;
 
       const step = 5;
-      let newPosition: { x: number; y: number };
+      const rotationStep = 5;
 
-      switch (e.key) {
-        case "ArrowUp":
-          newPosition = { x: carRef.current.x(), y: carRef.current.y() - step };
-          break;
-        case "ArrowDown":
-          newPosition = { x: carRef.current.x(), y: carRef.current.y() + step };
-          break;
-        case "ArrowLeft":
-          newPosition = { x: carRef.current.x() - step, y: carRef.current.y() };
-          break;
-        case "ArrowRight":
-          newPosition = { x: carRef.current.x() + step, y: carRef.current.y() };
-          break;
-        default:
-          return;
+      if (keysPressed.current["ArrowUp"]) {
+        const angle = carRef.current.rotation() * (Math.PI / 180);
+        const newPosition = {
+          x: carRef.current.x() + step * Math.sin(angle),
+          y: carRef.current.y() - step * Math.cos(angle),
+        };
+        carRef.current.position(newPosition);
       }
 
-      carRef.current.position(newPosition);
+      if (keysPressed.current["ArrowDown"]) {
+        const angle = carRef.current.rotation() * (Math.PI / 180);
+        const newPosition = {
+          x: carRef.current.x() - step * Math.sin(angle),
+          y: carRef.current.y() + step * Math.cos(angle),
+        };
+        carRef.current.position(newPosition);
+      }
+
+      if (keysPressed.current["ArrowLeft"]) {
+        carRef.current.rotation(carRef.current.rotation() - rotationStep);
+      }
+
+      if (keysPressed.current["ArrowRight"]) {
+        carRef.current.rotation(carRef.current.rotation() + rotationStep);
+      }
+
       carRef.current.getLayer()?.batchDraw();
     };
 
+    const interval = setInterval(moveCar, 1000 / 60); // 60 FPS
+
     window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
 
     return () => {
+      clearInterval(interval);
       window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
     };
   }, []);
 
@@ -66,14 +90,14 @@ const CarCanvas: React.FC = () => {
             alt="A car"
             ref={carRef}
             image={car}
-            x={window.innerWidth / 2}
-            y={window.innerHeight / 2}
+            x={0}
+            y={0}
             draggable
+            offsetX={car.width / 2}
+            offsetY={car.height / 2}
           />
         )}
       </Layer>
     </Stage>
   );
-};
-
-export default CarCanvas;
+}
