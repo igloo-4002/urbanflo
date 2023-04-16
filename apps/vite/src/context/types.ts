90e8b2cdb284d06c8161991c9ff3c912fe1bbf95
Taz17 <mutaz.ashhab@gmail.com>
2023-04-16 14:57:07 +1000

    update `canvasState.canvasItems`

--- apps/vite/src/context/types.ts
+++ apps/vite/src/context/types.ts
@@ -1,9 +1,34 @@
+import { type RefObject } from "react";
+import { type Image } from "react-konva";
+
+export enum CanvasItemType {
+  ROAD = "road",
+  CAR = "car",
+  TRAFFIC_LIGHT = "traffic-light",
+}
+
+export type CanvasItem = {
+  info: {
+    type: CanvasItemType;
+  };
+  props: {
+    alt: string;
+    ref?: RefObject<typeof Image>;
+    image: HTMLImageElement;
+    x: number;
+    y: number;
+    draggable: boolean;
+    offsetX: number;
+    offsetY: number;
+  };
+};
+
 export type AppState = {
   projectInfo: {
     name: string; // Normal Project settings, e.g. name, description, etc.
   };
   canvasState: {
-    canvasItems: JSX.Element[]; // Roads, Cars, traffic lights, etc.
+    canvasItems: CanvasItem[]; // Roads, Cars, traffic lights, etc.
   };
   projectState: {
     isSaved: boolean;
