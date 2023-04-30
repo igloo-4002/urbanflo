cd4f7838e5b7143fbe048846127548bea4ca90c1
elysiacasandra <egia0002@student.monash.edu>
2023-04-30 22:14:04 +1000

    Added t intersections to default app state

--- apps/vite/src/context/defaults.ts
+++ apps/vite/src/context/defaults.ts
@@ -1,12 +1,55 @@
-import { ModalViewNames, type AppState } from "./types";
+import {
+  CanvasItemType,
+  ModalViewNames,
+  type AppState,
+  type Road,
+} from "./types";
 
 export const getDefaultAppState: () => AppState = () => {
+  const road1: Road = {
+    info: {
+      type: CanvasItemType.ROAD,
+    },
+    props: {
+      alt: "Road 1",
+      image: new window.Image(),
+      x: 500,
+      y: 500,
+      draggable: true,
+      offsetX: 50,
+      offsetY: 50,
+    },
+    speedLimit: 60,
+    lanes: 2,
+    length: 200,
+    direction: "up",
+  };
+
+  const road2: Road = {
+    info: {
+      type: CanvasItemType.ROAD,
+    },
+    props: {
+      alt: "Road 2",
+      image: new window.Image(),
+      x: 420,
+      y: 420,
+      draggable: true,
+      offsetX: 50,
+      offsetY: 50,
+    },
+    speedLimit: 40,
+    lanes: 1,
+    length: 150,
+    direction: "left",
+  };
+
   return {
     projectInfo: {
       name: "untitled",
     },
     canvasState: {
-      canvasItems: [],
+      canvasItems: [road1, road2],
     },
     projectState: {
       isSaved: true, // when a user creates a project, it is saved by default
