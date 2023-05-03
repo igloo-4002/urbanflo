2f37c61ce2f4e2a61e370ee5883505a6a7f63869
dylanclements <58539346+dylanclements@users.noreply.github.com>
2023-05-03 17:21:53 +1000

    add demo and commonjs compiler options

---
+++ apps/sumo-server/src/index.ts
@@ -0,0 +1,27 @@
+import { exec } from "child_process";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.SUMO_PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.post("/demo", (_, res) => {
+  exec("../sumo/bin/sumo -c ./demo/demo.sumocfg", (error, stdout, stderr) => {
+    console.log(stdout);
+    console.error(stderr);
+
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send({ error: "Execution failed" });
+    }
+    res.send({ output: stdout });
+  });
+});
