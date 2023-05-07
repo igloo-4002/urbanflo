0b97255f6e8f73ef8d56988752a491f38d81be93
elysiacasandra <egia0002@student.monash.edu>
2023-05-07 18:59:38 +1000

    More homepage updates

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 
 import UrbanFloBlack from "../../public/UrbanFloBlack.png";
 import CanvasImg from "../../public/canvas.png";
+import TrafficImg from "../../public/traffic.jpeg";
 
 const navigation = [
   { name: "Product", href: "#" },
@@ -241,7 +242,7 @@ export default function Example() {
       </div>
       <div className="bg-white">
         <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="bg-#FE747E relative isolate overflow-hidden bg-slate-600 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
               viewBox="0 0 1024 1024"
               className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -256,21 +257,17 @@ export default function Example() {
               />
               <defs>
                 <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                  <stop stopColor="#7775D6" />
-                  <stop offset={1} stopColor="#E935C1" />
+                  <stop stopColor="#F7B326" />
+                  <stop offset={1} stopColor="#F7B326" />
                 </radialGradient>
               </defs>
             </svg>
             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Boost your productivity.
+                Urban plan better.
                 <br />
                 Start using our app today.
               </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
-                Malesuada adipiscing sagittis vel nulla.
-              </p>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
                 <a
                   href="#"
@@ -289,7 +286,7 @@ export default function Example() {
             <div className="relative mt-16 h-80 lg:mt-8">
               <img
                 className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                src={TrafficImg.src}
                 alt="App screenshot"
                 width={1824}
                 height={1080}
