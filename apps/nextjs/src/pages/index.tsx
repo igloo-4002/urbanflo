d732142af9990f0672dbf9c0e54cf45db01195ec
elysiacasandra <egia0002@student.monash.edu>
2023-05-07 18:43:01 +1000

    Updating homepage of landing page

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -7,30 +7,32 @@ import {
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 
+import UrbanFloBlack from "../../public/UrbanFloBlack.png";
+import CanvasImg from "../../public/canvas.png";
+
 const navigation = [
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "Contact us", href: "#" },
 ];
 
 const features = [
   {
-    name: "Push to deploy.",
+    name: "Canvas:",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "Create a network and run your simulation! You can change the properties of roads, inflows and intersections and add shapes and text to label wherever you need.",
     icon: CloudArrowUpIcon,
   },
   {
-    name: "SSL certificates.",
+    name: "Projects:",
     description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      "Save, import and export files in an organised matter with our project file system",
     icon: LockClosedIcon,
   },
   {
-    name: "Database backups.",
+    name: "Account:",
     description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      "Create an account to save your models to. Who knows when you will need to refer back to them!",
     icon: ServerIcon,
   },
 ];
@@ -49,11 +51,7 @@ export default function Example() {
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=orange&shade=600"
-                  alt=""
-                />
+                <img className="h-8 w-auto" src={UrbanFloBlack.src} alt="" />
               </a>
             </div>
             <div className="flex lg:hidden">
@@ -155,7 +153,7 @@ export default function Example() {
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Announcing our next round of funding.{" "}
+                Urban planners are loving our site.{" "}
                 <a href="#" className="font-semibold text-orange-600">
                   <span className="absolute inset-0" aria-hidden="true" />
                   Read more <span aria-hidden="true">&rarr;</span>
@@ -164,12 +162,11 @@ export default function Example() {
             </div>
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Data to enrich your online business
+                An urban traffic planner to fix our roads
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+                A web based tool for urban planners to model road networks to
+                find the most efficient network structure.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
@@ -207,15 +204,14 @@ export default function Example() {
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
                 <h2 className="text-base font-semibold leading-7 text-orange-600">
-                  Deploy faster
+                  Model accurately
                 </h2>
                 <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  A better workflow
+                  Our product
                 </p>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
+                  Create your models on a canvas and see them come to life with
+                  the click of a button.
                 </p>
                 <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                   {features.map((feature) => (
@@ -234,9 +230,9 @@ export default function Example() {
               </div>
             </div>
             <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+              src={CanvasImg.src}
               alt="Product screenshot"
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+              className="w-[48rem] max-w-none rounded-xl shadow-2xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
               width={2432}
               height={1442}
             />
