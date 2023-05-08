be9f02c22fbe4b5084bafc8bc99a19f9a5e3c7b3
Dylan Clements <dylanclements@dyn-118-139-32-35.its.monash.edu.au>
2023-05-08 10:45:09 +1000

    run sumo

--- apps/sumo-server/src/index.ts
+++ apps/sumo-server/src/index.ts
@@ -1,10 +1,23 @@
+import { exec } from "child_process";
 import express, { type Request, type Response } from "express";
 
 const app = express();
 const port = process.env.SUMO_PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+const cwd = process.cwd();
+
+const command = `${cwd}/sumo-files/sumo/bin/sumo -c ${cwd}/sumo-files/demo/demo.sumocfg`;
+
+app.get("/", (_: Request, res: Response) => {
+  exec(command, (error, stdout, stderr) => {
+    if (error || stderr) {
+      res.status(500).send({ error, stderr });
+    } else {
+      res.send({
+        data: stdout.split(/\r\n|\r|\n/).map((step) => step.trim()),
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
