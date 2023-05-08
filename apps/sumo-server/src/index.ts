186f62fe40d8e731654d8f59540fc1f158d10478
Dylan Clements <dylanclements@dyn-118-139-32-35.its.monash.edu.au>
2023-05-08 11:31:29 +1000

    add file uploads to simulation endpoint

--- apps/sumo-server/src/index.ts
+++ apps/sumo-server/src/index.ts
@@ -1,24 +1,40 @@
 import { exec } from "child_process";
 import express, { type Request, type Response } from "express";
+import multer from "multer";
 
 const app = express();
 const port = process.env.SUMO_PORT || 3000;
 
-const cwd = process.cwd();
-
-const command = `${cwd}/sumo-files/sumo/bin/sumo -c ${cwd}/sumo-files/demo/demo.sumocfg`;
-
-app.get("/", (_: Request, res: Response) => {
-  exec(command, (error, stdout, stderr) => {
-    if (error || stderr) {
-      res.status(500).send({ error, stderr });
-    } else {
-      res.send({
-        data: stdout.split(/\r\n|\r|\n/).map((step) => step.trim()),
-      });
-    }
-  });
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, `${process.cwd()}/uploads/`);
+  },
+  filename: (_req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}}`);
+  },
 });
+
+const uploads = multer({ storage });
+
+const command = `${process.cwd()}/sumo-files/sumo/bin/sumo -c ${process.cwd()}/sumo-files/demo/demo.sumocfg`;
+app.post(
+  "/simulation",
+  uploads.fields([
+    { name: "network", maxCount: 1 },
+    { name: "routes", maxCount: 1 },
+  ]),
+  (_req: Request, res: Response) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error || stderr) {
+        res.status(500).send({ error, stderr });
+      } else {
+        res.send({
+          data: stdout.split(/\r\n|\r|\n/).map((step) => step.trim()),
+        });
+      }
+    });
+  },
+);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
