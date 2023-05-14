c07045c060354acdacb4df4ec45ca7b7e8023b77
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-05-14 14:13:03 +1000

    Reverse product and model accurately

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -222,10 +222,10 @@ export default function Example() {
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
                 <h2 className="text-base font-semibold leading-7 text-orange-600">
-                  Model accurately
+                  Our product
                 </h2>
                 <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  Our product
+                  Model easily and accurately
                 </p>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
                   Create your models on a canvas and see them come to life with
