222b498cfa579a028a6124f01433bf6d64c01ab4
Taz17 <mutaz.ashhab@gmail.com>
2023-05-15 15:31:56 +1000

    add initial utils for rendering canvas items

---
+++ apps/vite/src/components/CanvasItems/util.tsx
@@ -0,0 +1,48 @@
+import {
+  CanvasItemType,
+  type CanvasItemTypes,
+  type Car,
+  type Intersection,
+  type Road,
+} from "../../context/types";
+import { Car as CarComponent } from "./Car";
+
+export function isRoad(canvasItem: CanvasItemTypes): canvasItem is Road {
+  return canvasItem.info.type === CanvasItemType.ROAD;
+}
+
+export function isIntersection(
+  canvasItem: CanvasItemTypes,
+): canvasItem is Intersection {
+  return canvasItem.info.type === CanvasItemType.TRAFFIC_LIGHT;
+}
+
+export function isCar(canvasItem: CanvasItemTypes): canvasItem is Car {
+  return canvasItem.info.type === CanvasItemType.CAR;
+}
+
+type CanvasItemRenderElement<T> = {
+  element: T;
+  index: number;
+};
+
+function filterCanvasItems<T>(
+  canvasItems: CanvasItemTypes[],
+  predicate: (item: CanvasItemTypes) => boolean,
+): CanvasItemRenderElement<T>[] {
+  const items: CanvasItemRenderElement<T>[] = [];
+
+  for (let i = 0; i < canvasItems.length; i++) {
+    if (predicate(canvasItems[i])) {
+      items.push({ element: canvasItems[i] as T, index: i });
+    }
+  }
+
+  return items;
+}
+
+export function renderCanvasItems(canvasItems: CanvasItemTypes[]) {
+  const cars = filterCanvasItems<Car>(canvasItems, isCar);
+  const roads = filterCanvasItems<Road>(canvasItems, isRoad);
+  const intersections = filterCanvasItems<Road>(canvasItems, isIntersection);
+}
