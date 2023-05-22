4a43309d0e0995ff6a5f76f34c62a410a2f10b49
Vishal Jakhar <vishaljakhar60@yahoo.com>
2023-05-22 10:26:58 +1000

    remove unnecessary mapping

--- apps/vite/src/components/Canvas.tsx
+++ apps/vite/src/components/Canvas.tsx
@@ -25,41 +25,21 @@ export default function Canvas({
   color: string;
 }) {
   const nodes = [
-    { id: "start", x: 0.0, y: 0.0 },
-    { id: "end", x: 400.0, y: 0.0 },
+    { id: "south", x: 0.0, y: -100.0 },
+    { id: "north", x: 0.0, y: 100.0 },
+    { id: "east", x: 100.0, y: 0.0 },
+    { id: "center", x: 0.0, y: 0.0 },
   ];
 
-  const edges = [{ id: "start_end", from: "start", to: "end" }];
+  const edges = [
+    { id: "center_south", from: "center", to: "south" },
+    { id: "center_north", from: "center", to: "north" },
+    { id: "east_center", from: "east", to: "center" },
+  ];
 
   const getNodeById = (id: string) => nodes.find((node) => node.id === id);
 
-  const carPositions = [
-    { x: 41.49294905988499, y: -4.8 },
-    { x: 38.017991663981235, y: -4.8 },
-    { x: 92.30788270906547, y: -4.8 },
-    { x: 53.502549675642506, y: -4.8 },
-    { x: 162.83601074482436, y: -4.8 },
-    { x: 26.089717962499705, y: -4.8 },
-    { x: 55.32929933872075, y: -4.8 },
-    { x: 11.068741357768886, y: -4.8 },
-    { x: 48.57433211205061, y: -4.8 },
-    { x: 48.97933477631304, y: -4.8 },
-    { x: 123.5905698460533, y: -4.8 },
-    { x: 112.24469129420913, y: -4.8 },
-    { x: 43.23254432287067, y: -4.8 },
-    { x: 91.25295146781382, y: -4.8 },
-    { x: 80.94091290933798, y: -4.8 },
-    { x: 15.501278712740167, y: -4.8 },
-    { x: 11.602273806906306, y: -4.8 },
-    { x: 157.4464569345004, y: -4.8 },
-    { x: 174.7426710002455, y: -4.8 },
-    { x: 5.1, y: -4.8 },
-    { x: 112.5946889223383, y: -4.8 },
-    { x: 151.88847698923612, y: -4.8 },
-    { x: 90.83337086792969, y: -4.8 },
-    { x: 132.3576746030624, y: -4.8 },
-    { x: 6.476679887389763, y: -4.8 },
-  ];
+  const redDotCoordinates = mapCoordinates(x, y);
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
@@ -102,19 +82,12 @@ export default function Canvas({
           );
         })}
 
-        {carPositions.map((position, index) => {
-          const redDotCoordinates = mapCoordinates(x, y);
-
-          return (
-            <Circle
-              key={index}
-              x={redDotCoordinates.x}
-              y={redDotCoordinates.y}
-              radius={10} // Adjust for visibility
-              fill={color}
-            />
-          );
-        })}
+        <Circle
+          x={redDotCoordinates.x}
+          y={redDotCoordinates.y}
+          radius={10} // Adjust for visibility
+          fill={color}
+        />
       </Layer>
     </Stage>
   );
