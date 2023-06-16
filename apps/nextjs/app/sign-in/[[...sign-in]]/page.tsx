837bbd75aa9f587888b3dca0fe83aee19a9ea59b
Dylan Clements <dylan.clements99@gmail.com>
2023-06-16 13:59:14 +1000

    made root path public

--- apps/nextjs/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/nextjs/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex h-screen flex-row items-center justify-center border">
+      <SignIn />
+    </div>
+  );
 }
