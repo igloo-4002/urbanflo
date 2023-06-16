62e6acd480eda0e9291086382de660f1e51c11af
Dylan Clements <dylan.clements99@gmail.com>
2023-06-16 17:37:56 +1000

    remove posts

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -4,7 +4,4 @@ export const authRouter = router({
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.auth.session;
   }),
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can see this secret message!";
-  }),
 });
