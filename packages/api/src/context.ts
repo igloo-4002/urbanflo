9ef7569e85b07d3c7d06da5c030f7e546b312be2
Dylan Clements <dylan.clements99@gmail.com>
2023-06-16 17:20:10 +1000

    add clerk to api package

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -1,11 +1,12 @@
-import { prisma } from "@acme/db";
-import { type inferAsyncReturnType } from "@trpc/server";
-import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import { getAuth } from "@clerk/nextjs/server";
 import type {
   SignedInAuthObject,
   SignedOutAuthObject,
 } from "@clerk/nextjs/api";
+import { getAuth } from "@clerk/nextjs/server";
+import { type inferAsyncReturnType } from "@trpc/server";
+import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+
+import { prisma } from "@igloo/db";
 
 /**
  * Replace this with an object if you want to pass things to createContextInner
